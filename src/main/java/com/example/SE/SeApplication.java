fb8f96ced3674372c6f3d859b4cdb4577baf813d
bincoi03 <Phuc.LM210687@sis.hust.edu.vn>
2023-05-06 19:33:25 +0700

    Add Environment

---
+++ src/main/java/com/example/SE/SeApplication.java
@@ -0,0 +1,13 @@
+package com.example.SE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeApplication.class, args);
+	}
+
+}
