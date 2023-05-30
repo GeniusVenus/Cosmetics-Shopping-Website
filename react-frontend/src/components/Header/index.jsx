e88e344edf91d4fe2e94618c8e6c8fbfc7a3bd77
Phan Khoi Nguyen 20210652 <khoinguyen6103@gmail.com>
2023-05-30 11:19:16 +0700

    refactor

--- react-frontend/src/components/Header/index.jsx
+++
@@ -1,8 +0,0 @@
-import './styles.scss';
-export default function Header() {
-    return (
-        <div>
-            <p className="header">This is header</p>
-        </div>
-    );
-}
--- react-frontend/src/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import UserReducer from "./features/User/UserSlice";
-const store = configureStore({
-  reducer: {
-    user: UserReducer,
-  },
-});
-
-export default store;
