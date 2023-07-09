8075626cb2b8f96223c8df32d8068e40dbddd8ee
bincoi03 <Phuc.LM210687@sis.hust.edu.vn>
2023-07-10 03:32:24 +0700

    repair delete mapping

--- springboot-backend/src/main/java/com/example/SE/controller/ProductController.java
+++ springboot-backend/src/main/java/com/example/SE/controller/ProductController.java
@@ -47,7 +47,7 @@ public class ProductController {
         }
     }
 
-    @DeleteMapping("/tutorials")
+    @DeleteMapping("/deleteAll")
     public ResponseEntity<HttpStatus> deleteAllProducts() {
         try {
             productService.deleteAllProducts();
